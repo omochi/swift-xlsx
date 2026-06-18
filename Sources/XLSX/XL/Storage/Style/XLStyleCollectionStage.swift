f9dbe0189ae60dc84416afe1a6a8c2d19a8eef61
omochimetaru <omochi.metaru@gmail.com>
2026-06-18 13:53:21 +0900

    comment for case order

--- Sources/XLSX/XL/Storage/Style/XLStyleCollectionStage.swift
+++ Sources/XLSX/XL/Storage/Style/XLStyleCollectionStage.swift
@@ -1,3 +1,5 @@
+// style の collect は CaseIterable の定義順で実行される。
+// cellFormats のような依存側の record は、参照先の storage より後ろに置く。
 public enum XLStyleCollectionStage: CaseIterable {
     case numberFormats
     case fonts
